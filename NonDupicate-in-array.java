67665140c5c9fc213c1d267306958073ecbe87dc
MarcHines <mxh857@miami.edu>
2017-03-13 23:35:29 -0400

    Update NonDupicate-in-array.java

--- NonDupicate-in-array.java
+++ NonDupicate-in-array.java
@@ -1,6 +1,9 @@
 /*
 Given an array of integer where each integer appears twice except for one integer return the non duplicated integer 
 ie. array = {1,1,3,3,4,55,55,2,3,3,2} Result: 4
+Of course we could use a map to count the occurences of each integer...blah blah blah, but thats not cool.
+Using XOR we can accomplish the solution. Anythig XOR with itself cancels out. We are then left with the
+not duplicated value
 */
 
 
